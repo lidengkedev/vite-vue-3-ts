2be6f8c66424302d19432c86aea6f392f64c6f5a
ldk <lidengke0221@126.com>
2024-03-06 11:30:56 +0800

    update

--- src/views/bill-page.vue
+++ src/views/bill-page.vue
@@ -1,55 +1,84 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import CompDialog from '@/components/comp-dialog.vue'
 
 interface ITableItem {
+  id: number,
   name: string,
   price: number | string,
   date: string,
   remark: string
 }
 
-let search = reactive({ name: '', year: '', month: '', day: '', remark: '' })
-let form = reactive({ name: '', price: '', date: '', remark: '' })
+let search = ref({ name: '', year: '', month: '', day: '', remark: '' })
+let form = ref({ id: 0, name: '', price: '', date: '', remark: '' })
 let tableData: ITableItem[] = reactive([])
+const is_show_dialog = ref(false)
+const form_type = ref('add')
+
+let totalPrice = ref(0)
+/** 监听 表格变化 */
+watch(tableData, (newVal) => {
+  totalPrice.value = 0
+  newVal.map((item) => {
+    totalPrice.value = totalPrice.value + Number(item.price)
+    return item
+  })
+  totalPrice.value = Number(totalPrice.value.toFixed(2))
+})
+
+/** 查询 */
 const handleSearch = () => {
   console.log(search)
 }
-const is_show_dialog = ref(false)
-const form_type = ref('add')
+/** 重置按钮 */
 const handleReset = () => {
-  search.name = ''
-  search.year = ''
-  search.month = ''
-  search.day = ''
-  search.remark = ''
+  search.value.name = ''
+  search.value.year = ''
+  search.value.month = ''
+  search.value.day = ''
+  search.value.remark = ''
 }
+/** 新增 */
 const handleAdd = () => {
   is_show_dialog.value = true
   form_type.value = 'add'
 }
+/** 更新弹框状体 */
 const handleUpdate = (value: boolean) => {
   is_show_dialog.value = value
 }
+/** 弹框 确认 */
 const handleSure = () => {
   is_show_dialog.value = false
   if (form_type.value === 'add') {
-    tableData.push(form)
+    // 不能直接赋值，否则tableData的所有行数据的引用地址变成同一个，改一行数据，所有行都会改变
+    tableData.push({...form.value, id: Date.now()})
+  } if (form_type.value === 'edit') {
+    tableData[tableData.findIndex(v => v.id === form.value.id)] = {...form.value}
   }
 }
+/** 弹框 取消 */
 const handleCancel = () => {
   is_show_dialog.value = false
 }
+/** 修改 */
 const handleEdit = (row: ITableItem) => {
-  form.name = row.name
-  form.price = String(row.price)
-  form.date = row.date
-  form.remark = row.remark
+  form.value.name = row.name
+  form.value.price = String(row.price)
+  form.value.date = row.date
+  form.value.remark = row.remark
+  form.value.id = row.id
   is_show_dialog.value = true
   form_type.value = 'edit'
 }
+/** 删除 */
 const handleDelete = (row: ITableItem, index: number) => {
   tableData.splice(index, 1)
+}
+// 导出
+const handleExport = () => {
+  alert('导出')
 }
 </script>
 
@@ -81,6 +110,7 @@ const handleDelete = (row: ITableItem, index: number) => {
         <button data-type="primary" class="search" @click="handleSearch">查询</button>
         <button data-type="text" class="search" @click="handleReset">重置条件</button>
         <button data-type="primary" class="search" @click="handleAdd">新增</button>
+        <button data-type="primary" class="search" @click="handleExport">导出</button>
       </div>
     </div>
     <div class="table-warpper">
@@ -122,6 +152,26 @@ const handleDelete = (row: ITableItem, index: number) => {
               <button data-type="text" @click="handleEdit(td)">修改</button>
               <button data-type="text" @click="handleDelete(td, index)">删除</button>
             </td>
+          </tr>
+        </table>
+      </div>
+      <div class="table-footer__warpper">
+        <table border="1" class="table-body">
+          <colgroup>
+            <col width="60">
+            <col width="100">
+            <col width="100">
+            <col width="100">
+            <col width="100">
+            <col width="100">
+          </colgroup>
+          <tr>
+            <td align="center">-</td>
+            <td>-</td>
+            <td align="right">{{ totalPrice }}</td>
+            <td align="center">-</td>
+            <td>-</td>
+            <td>-</td>
           </tr>
         </table>
       </div>
@@ -183,7 +233,7 @@ const handleDelete = (row: ITableItem, index: number) => {
   width: 100%;
 }
 .table-body__warpper {
-  height: 75vh;
+  height: 70vh;
   overflow: hidden auto;
 }
 .form-warpper .form-inner {
