7bdf99b8e641c67edd737c88a8f46817a31d04aa
ldk <lidengke0221@126.com>
2024-03-06 19:01:46 +0800

    update

--- src/views/bill-upload.vue
+++ src/views/bill-upload.vue
@@ -2,12 +2,84 @@
 import router from '@/router';
 import { reactive } from 'vue';
 import UploadFile from '@/components/upload-file.vue'
+import { readFile, read, utils } from 'xlsx'
 
-let tableData: { name: string }[] = reactive([])
+let tableData: { name: string, _data: object[] }[] = reactive([])
 
 /** 导入 */
-const handleUpload = (file: File) => {
-  tableData.push({ name: file.name })
+const handleUpload = async (file: File) => {
+  const data = await file.arrayBuffer()
+  const workbook = read(data)
+  const sheet = workbook.SheetNames
+  let file_data: any = []
+  let table_data: any = []
+  for (let i = 0; i < sheet.length; i++) {
+    file_data = utils.sheet_to_json(workbook.Sheets[sheet[i]])
+    console.log(file_data)
+    for (let r = 0; r < file_data.length; r++) {
+      const item = file_data[r]
+      let row: any = { index: r, id: '', date: '', productName: '', price: '', num: '', unit: '', sole: '', productType: '', applyType: '', total: '', remark: '' }
+      if (i === 9 && r === 2) {
+        // eslint-disable-next-line no-debugger
+        debugger
+      }
+      // if (item.__EMPTY) {
+      if (item['序号']) {
+        row.id = item['序号']
+      // } else if (item.__EMPTY_1) {
+      }
+      if (item['日期']) {
+        row.date = item['日期']
+      }
+      if (item['商品名称']) {
+        row.productName = item['商品名称']
+      }
+      if (item['单价']) {
+        row.price = item['单价']
+      }
+      if (item['数量']) {
+        row.num = item['数量']
+      }
+      if (item['单位']) {
+        row.unit = item['单位']
+      }
+      if (item['折扣']) {
+        row.sole = item['折扣']
+      }
+      if (item['商品类型']) {
+        row.productType = item['商品类型']
+      }
+      if (item['支付方式']) {
+        row.applyType = item['支付方式']
+      }
+      if (item['金额']) {
+        row.total = item['金额']
+      }
+      if (item['备注']) {
+        row.remark = item['备注']
+      }
+      if (item['月份']) {
+        row.month = item['月份']
+      }
+      if (item.__EMPTY_12) {
+        row.other12 = item.__EMPTY_12
+      }
+      if (item.__EMPTY_13) {
+        row.other13 = item.__EMPTY_13
+      }
+      if (item.__EMPTY_14) {
+        row.other14 = item.__EMPTY_14
+      }
+      if (item.__EMPTY_15) {
+        row.other15 = item.__EMPTY_15
+      }
+      // console.log(row)
+      table_data.push(row)
+    }
+  }
+  const table_header_keys = ['序号', '日期', '商品名称', '单价', '数量', '单位', '折扣', '商品类型', '支付方式', '金额', '备注']
+  console.log(table_data)
+  tableData.push({ name: file.name, _data: file_data })
 }
 /** 查看 */
 const handleInfo = (index: number) => {
@@ -24,8 +96,6 @@ const handleDelete = (index: number) => {
   <div class="report-import">
     <div class="form-warpper">
       <div class="form-item">
-        <!-- <input type="file" name="">
-        <button data-type="primary" @click="handleImport">导入</button> -->
         <upload-file accept=".xlsx,.xls" @on-upload="handleUpload"></upload-file>
       </div>
     </div>
